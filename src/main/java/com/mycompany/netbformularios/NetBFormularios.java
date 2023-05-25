dae3f776d4fd06224eb26e7bd6bf71292094adde
MauroMoyano <mauromoyanodev@gmail.com>
2023-05-25 06:01:59 -0300

    First commit for practice

---
+++ src/main/java/com/mycompany/netbformularios/NetBFormularios.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.netbformularios;
+
+/**
+ *
+ * @author yogu_
+ */
+public class NetBFormularios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Formulario ventana = new Formulario();
+        ventana.setVisible(true);
+    }
+}
